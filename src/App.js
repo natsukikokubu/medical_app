29a2516214408d88bbbdd0fa6114f6501f01b171
natsukikokubu <natsukikokubu3@gmail.com>
2023-08-20 22:34:37 +0900

    top画面を作成

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>意思決定支援アプリ</h1>
+        <h2>あなたはどこまで治療しますか？</h2>
+        <h3>元気なうちに選択しておこう！</h3>
+      </header>
+      <button>はじめる</button>
+    </div>
+  );
+}
+
+export default App;
