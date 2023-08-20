5dc751a06ef3c71935fe1877430d017981a9883a
natsukikokubu <natsukikokubu3@gmail.com>
2023-08-21 01:25:16 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>意思決定支援アプリ</h1>
-        <h2>あなたはどこまで治療しますか？</h2>
-        <h3>元気なうちに選択しておこう！</h3>
-      </header>
+    <>
+      <h1>意思決定支援アプリ</h1>
+      <p>あなたはどこまで治療しますか？</p>
+      <p>元気なうちに選択しておこう！</p>
+
       <button>はじめる</button>
-    </div>
+    </>
   );
 }
 
